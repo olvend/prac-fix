8d568e1f6e6bb4e3468ddc9449b7302d8bbfb179
olvend <olve@nystad.net>
2026-03-17 16:03:27 +0100

    Add title and done button to ConfigScreen

--- src/main/java/io/github/olvend/pracfix/gui/ConfigScreen.java
+++ src/main/java/io/github/olvend/pracfix/gui/ConfigScreen.java
@@ -1,17 +1,37 @@
 package io.github.olvend.pracfix.gui;
 
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
+
+import java.io.IOException;
 
 public class ConfigScreen extends GuiScreen {
     private final GuiScreen parentScreen;
+    private final String title = "Prac Fix Config";
 
     public ConfigScreen(GuiScreen parentScreen) {
         this.parentScreen = parentScreen;
     }
 
     @Override
+    public void initGui() {
+        buttonList.add(new GuiButton(200, width / 2 - 100, height / 6 + 168, I18n.format("gui.done")));
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        if (!button.enabled) return;
+
+        if (button.id == 200) {
+            mc.displayGuiScreen(parentScreen);
+        }
+    }
+
+    @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
+        drawCenteredString(fontRendererObj, title, width / 2, 15, 0xFFFFFF);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 }
